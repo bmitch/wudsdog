bf8fd59b11fc8624fd8060cc055aeb3ee7d976ba
Bill Mitchell <wkmitch@gmail.com>
2016-06-15 23:18:13 +0100

    Fixes #6

--- app/Probe.php
+++ app/Probe.php
@@ -7,22 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Probe extends Model
 {
 
-	protected $fillable = [
-		"timestamp",
-		"macAddress",
-		"signalStrength",
-		"ssid",
-		"manufacturerName",
-		"userId",
-	];
+    protected $fillable = [
+        "timestamp",
+        "macAddress",
+        "signalStrength",
+        "ssid",
+        "manufacturerName",
+        "userId",
+    ];
 
-	protected $table = 'probes';
+    protected $table = 'probes';
 
-	public static function create(array $attributes = [])
-	{
-		$providedKey = request()->header()['key'][0];
-		$attributes['userId'] = User::byApiKey($providedKey)[0]->id;
-		return parent::create($attributes);
-	}
+    public static function create(array $attributes = [])
+    {
+        $providedKey = request()->header()['key'][0];
+        $attributes['userId'] = User::byApiKey($providedKey)[0]->id;
+        return parent::create($attributes);
+    }
 
 }
